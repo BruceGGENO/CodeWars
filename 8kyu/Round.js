5b2cd7d81d6968b26ea6deb31245e3b158f685e5
Bruce Geno <brucegeno34@gmail.com>
2026-04-27 16:56:05 +0200

    Adding kata

---
+++ 8kyu/Round.js
@@ -0,0 +1,8 @@
+function roundIt(n) {
+    //coding here...
+    let arr = n.toString().split(".");
+    if (arr[0].length < arr[1].length) return Math.ceil(n)
+    else if (arr[0].length > arr[1].length) return Math.floor(n)
+    else return Math.round(n)
+
+}
