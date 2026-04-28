048ddf11acad49a1d5a58c16c395675dd4f73f4a
Bruce Geno <brucegeno34@gmail.com>
2026-04-28 19:12:42 +0200

    Change to AddArrays

--- 6kyu/AddArrays.js
+++ 6kyu/AddArrays.js
@@ -1,4 +1,4 @@
 function addArrays(array1, array2) {
-    return array1.length === 0 && array2.length === 0 ? [] : (Number(array1.join('')) + Number(array2.join(''))).toString().split("");
+    return array1.length === 0 && array2.length === 0 ? [] : (parseInt(array1.join('')) + parseInt(array2.join(''))).toString().split("");
 
 }
