6ede94044092a8574642fd73a1ed71bd71a416c4
Bruce Geno <brucegeno34@gmail.com>
2026-05-04 16:29:44 +0200

    Adding console log to findOutlier

--- 6kyu/FindOutlier.js
+++ 6kyu/FindOutlier.js
@@ -3,3 +3,4 @@ function findOutlier(integers) {
     return integers.filter(num => num % 2 === 0).length > 1 ? Number(integers.filter(num => num % 2 !== 0)) : Number(integers.filter(num => num % 2 === 0))
 
 }
+console.log(findOutlier([1, 3, 5, 6, 2]))
