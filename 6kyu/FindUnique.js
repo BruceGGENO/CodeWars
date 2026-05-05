c0b88e2e47e5b73fc94da7202539f9974d1d1835
Bruce Geno <brucegeno34@gmail.com>
2026-05-05 18:21:37 +0200

    Better solution for FindUnique.js

--- 6kyu/FindUnique.js
+++ 6kyu/FindUnique.js
@@ -1,13 +1,3 @@
 function findUnique(numbers) {
-    let result = []
-    for (let i = 0; i < numbers.length; i++) {
-        for (let j = 0; j < numbers.length; j++) {
-            if (numbers[i] === numbers[j] && i != j) {
-                result.push(numbers[j]);
-            }
-        }
-    }
-    for (let i = 0; i < numbers.length; i++) {
-        if (!result.includes(numbers[i])) return numbers[i]
-    }
+    return numbers.reduce((a, b) => a ^ b);
 }
