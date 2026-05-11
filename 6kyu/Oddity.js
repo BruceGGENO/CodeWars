af1abf3e2ddcb456e9875fe4ef0f62d48b07d2a3
Bruce Geno <brucegeno34@gmail.com>
2026-05-11 17:29:55 +0200

    Better solution for Oddity.js

--- 6kyu/Oddity.js
+++ 6kyu/Oddity.js
@@ -1,8 +1,5 @@
 function oddity(n) {
-    let count = 0;
-    for (let i = 1; i <= n; i++) {
-        if (n % i === 0) count += 1;
-    }
-    return count % 2 === 0 ? 'even' : 'odd';
+    return Math.sqrt(n) === Math.floor(Math.sqrt(n)) ? 'odd' : 'even';
+    //The theory is if a number's square root is a perfect square it has an 'odd' number of divisors
 }
 console.log(oddity(12))
