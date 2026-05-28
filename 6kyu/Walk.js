e4a62a284cdc7a9279cbc36ff8f8b60195f36a3c
Bruce Geno <brucegeno34@gmail.com>
2026-05-28 10:52:49 +0200

    Using Boolean instead of ternary operator

--- 6kyu/Walk.js
+++ 6kyu/Walk.js
@@ -1,5 +1,5 @@
 function isValidWalk(walk) {
     let time = Math.abs((walk.join("").match(/n/gi)||[]).length - (walk.join("").match(/s/gi)||[]).length) + Math.abs((walk.join("").match(/w/gi)||[]).length - (walk.join("").match(/e/gi)||[]).length)
-    return time===0 && walk.length===10 ? true : false;
+    return Boolean(time===0 && walk.length===10);
 }
 console.log(isValidWalk(['n','n','n','s','n','s','n','s','e','s']))
