08dd60f91b1a3d87930f55057b69d5c37938a0df
Bruce Geno <brucegeno34@gmail.com>
2026-06-01 13:58:33 +0200

    Final solution for UniqueOrder

--- 6kyu/UniqueOrder.js
+++ 6kyu/UniqueOrder.js
@@ -1,10 +1,5 @@
 var uniqueInOrder = function (iterable) {
     let inp = typeof iterable === 'string' ? iterable.split('') : iterable;
-    for (let i = 0; i < inp.length; i++) {
-        if (inp[i] === inp[i + 1] || inp[i - 1] === inp[i]) inp.splice(i, 1)
-
-        console.log(inp.join(''))
-    }
-    //return inp.join('')
+    return inp.filter((i, ix) => i !== inp[ix - 1])
 }
 console.log(uniqueInOrder('AAAABBBCCDAABBB'))
