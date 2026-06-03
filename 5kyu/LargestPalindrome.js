9f297dbbdb014603f9c87a17e4885f60ee902a86
Bruce Geno <brucegeno34@gmail.com>
2026-06-03 09:26:07 +0200

    Solution for LargestPalindrome.js

--- 5kyu/LargestPalindrome.js
+++ 5kyu/LargestPalindrome.js
@@ -1,12 +1,14 @@
 function largestPalindromicProduct(lower, upper) {
-    let biggest=1
-    for(let i=lower;i<=upper;i++){
-        for(let j=i;j<=upper;j++){
-            let prod= Number(i * j)
-            let rev=prod.toString().split('').reverse.join('')
-            if(prod === i*j) biggest=prod
+    let biggest = NaN
+    let prod = 1
+    for (let i = lower; i <= upper; i++) {
+        for (let j = i; j <= upper; j++) {
+            prod = i * j
+            let rev = Number(prod.toString().split('').reverse().join(''))
+            if (prod === rev) biggest = prod
         }
     }
-    return prod
+
+    return biggest 
 }
-console.log(largestPalindromicProduct(1,3))
+console.log(largestPalindromicProduct(4, 10))
